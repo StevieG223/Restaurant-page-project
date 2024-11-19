eb6122dcae2acba11dba12e0e27edd877a6f9b5e
StevieG223 <77689111+StevieG223@users.noreply.github.com>
2024-11-19 15:12:46 -0500

    Add navbar, logo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import './style.css';
+import './styles.css';
+import Icon from './cowboy-hat-with-moustache-svgrepo-com.png';
+
+
+const myIcon = new Image();
+myIcon.src = Icon;
+myIcon.classList.add("cowboy-logo");
+
+const element = document.getElementById("cowboy-logo");
+element.append(myIcon);
+
+
+
+function addImage(classToAppend, source){
+    const myImage = new Image();
+    myImage.src = source;
+    element = document.getElementsByClassName(classToAppend);
+    element.append(myImage);
+}
+
